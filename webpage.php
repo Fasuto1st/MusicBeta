75f260d8514354d19adb29e92cc9a65dd1058813
Fasuto1st <nicha.tnct@gmail.com>
2020-12-05 01:30:23 +0700

    update

--- webpage.php
+++ webpage.php
@@ -4,8 +4,7 @@ include 'item.php';
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
-  <title>Music Player</title>
+  <title>Music Beta</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/style1.css">
